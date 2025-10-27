59e6da1aaf79995931275fa81bccc8ebed977b0e
JLuigi400 <jorgeluigi400@gmail.com>
2025-10-26 20:44:49 -0600

    [MODIFICA] agregando el KSP, Retrofit en los Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,18 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    // Para Integrar KSP
+    alias(libs.plugins.kotlin.ksp)
+
+    // Para ingregrar en el proyecto hilt
+    id("com.google.dagger.hilt.android")
+
+    //Plugins para Parcelizar
+    alias(libs.plugins.jetbrainsKotlinSerialization)
+    alias(libs.plugins.kotlin.parcelize)
+
+
 }
 
 android {
@@ -40,6 +52,30 @@ android {
 }
 
 dependencies {
+
+    // Para integrar Dagger Hilt y sus herramientas necesarias.
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.viewmodel)
+    ksp(libs.hilt.android.compilador)
+
+    val androidxhiltCompiler = "1.3.0" // Cambioar a 1.2.0
+    implementation("androidx.hilt:hilt-work:${androidxhiltCompiler}")
+    ksp("androidx.hilt:hilt-compiler:${androidxhiltCompiler}")
+
+    // Dependencia para Hilt y evitar errores extraños
+    implementation("androidx.work:work-runtime-ktx:2.7.0")
+
+    // Para instalar Retrofit
+    implementation(libs.okhttp)
+    implementation(libs.retrofit)
+    implementation(libs.gson.converter)
+    implementation(libs.login.interceptor)
+
+    // Implementa las librerias de Navigation Compose
+    implementation(libs.androidx.navigation.compose.android)
+
+    //Implementa las librerias de Kotlinx para parcelizar (o utilizar JSON)
+    implementation(libs.kotlinx.serialization.json)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
